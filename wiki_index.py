027ed25f2a0addb08619ba9d6e456818d1e9cc58
Richard So <richardso2021@gmail.com>
2025-04-07 01:45:48 -0400

    script for generating disk index for wiki corpus

---
+++ wiki_index.py
@@ -0,0 +1,271 @@
+import os
+import json
+import warnings
+import numpy as np
+from typing import cast
+import argparse
+import datasets
+from datasets import Dataset
+import torch
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModel
+
+import diskannpy
+
+def pooling(pooler_output, last_hidden_state, attention_mask=None, pooling_method="mean"):
+    if pooling_method == "mean":
+        last_hidden = last_hidden_state.masked_fill(~attention_mask[..., None].bool(), 0.0) # type: ignore
+        return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None] # type: ignore
+    elif pooling_method == "cls":
+        return last_hidden_state[:, 0]
+    elif pooling_method == "pooler":
+        return pooler_output
+    else:
+        raise NotImplementedError("Pooling method not implemented!")
+
+def load_model(model_path: str, use_fp16: bool = False):
+    model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
+    model.eval()
+    model.cuda()
+    if use_fp16:
+        model = model.half()
+    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True, trust_remote_code=True)
+
+    return model, tokenizer
+
+def load_corpus(corpus_path: str) -> Dataset:
+    corpus = datasets.load_dataset("json", data_files=corpus_path, split="train")
+    return corpus # type: ignore
+
+class DiskannIndexBuilder:
+    r"""
+    A tool class used to build a diskann index used in retrieval.
+    Assuming e5 retrieval model.
+    """
+
+    def __init__(
+        self,
+        model_path,
+        corpus_path,
+        save_dir,
+        max_length,
+        batch_size,
+        use_fp16,
+        search_mem_max,
+        pooling_method=None,
+        instruction=None,
+        embedding_path=None,
+        save_embedding=False,
+    ):
+
+        self.model_path = model_path
+        self.corpus_path = corpus_path
+        self.save_dir = save_dir
+        self.max_length = max_length
+        self.batch_size = batch_size
+        self.use_fp16 = use_fp16
+        self.search_mem_max = search_mem_max
+        self.instruction = instruction
+        self.embedding_path = embedding_path
+        self.save_embedding = save_embedding
+
+        # set instruction for encode
+        if self.instruction is not None:
+            self.instruction = self.instruction.strip() + " "
+            print("Set instruction for encoding:", self.instruction)
+        else:
+            self.instruction = "passage: " # assume e5
+            warnings.warn(f"Instruction is set to default: {self.instruction}")
+
+        #. config pooling method
+        if pooling_method is None:
+            try:
+                # read pooling method from 1_Pooling/config.json
+                pooling_config = json.load(open(os.path.join(self.model_path, "1_Pooling/config.json")))
+                for k, v in pooling_config.items():
+                    if k.startswith("pooling_mode") and v:
+                        pooling_method = k.split("pooling_mode_")[-1]
+                        if pooling_method == 'mean_tokens':
+                            pooling_method = 'mean'
+                        elif pooling_method == 'cls_token':
+                            pooling_method = 'cls'
+                        else:
+                            # raise warning: not implemented pooling method
+                            warnings.warn(f"Pooling method {pooling_method} is not implemented.", UserWarning)
+                            pooling_method = 'mean'
+                        break
+            except Exception as _:
+                print(f"Pooling method not found in {self.model_path}, use default pooling method (mean).")
+                # use default pooling method
+                pooling_method = 'mean'
+        else:
+            if pooling_method not in ['mean', 'cls', 'pooler']:
+                raise ValueError(f"Invalid pooling method {pooling_method}.")
+        self.pooling_method = pooling_method
+
+        self.gpu_num = torch.cuda.device_count()
+        # prepare save dir
+        print(self.save_dir)
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        else:
+            if not self._check_dir(self.save_dir):
+                warnings.warn("Some files already exists in save dir and may be overwritten.", UserWarning)
+
+        self.index_save_path = os.path.join(self.save_dir, f"e5_diskann_{self.search_mem_max}")
+        os.makedirs(self.index_save_path, exist_ok=True)
+
+        self.embedding_save_path = os.path.join(self.save_dir, "emb_e5.memmap")
+
+        self.corpus: Dataset = load_corpus(self.corpus_path)
+
+        print("Finish loading...")
+
+    @staticmethod
+    def _check_dir(dir_path):
+        r"""Check if the dir path exists and if there is content."""
+
+        if os.path.isdir(dir_path):
+            if len(os.listdir(dir_path)) > 0:
+                return False
+        else:
+            os.makedirs(dir_path, exist_ok=True)
+        return True
+
+    def build_index(self):
+        r"""Constructing different indexes based on selective retrieval method."""
+        self.build_dense_index()
+
+
+    def _load_embedding(self, embedding_path, corpus_size, hidden_size):
+        all_embeddings = np.memmap(embedding_path, mode="r", dtype=np.float32).reshape(corpus_size, hidden_size)
+        return all_embeddings
+
+    def _save_embedding(self, all_embeddings):
+        memmap = np.memmap(self.embedding_save_path, shape=all_embeddings.shape, mode="w+", dtype=all_embeddings.dtype)
+        length = all_embeddings.shape[0]
+        # add in batch
+        save_batch_size = 10000
+        if length > save_batch_size:
+            for i in tqdm(range(0, length, save_batch_size), leave=False, desc="Saving Embeddings"):
+                j = min(i + save_batch_size, length)
+                memmap[i:j] = all_embeddings[i:j]
+        else:
+            memmap[:] = all_embeddings
+
+    def encode_all(self):
+        all_embeddings = []
+        for start_idx in tqdm(range(0, len(self.corpus), self.batch_size), desc="Inference Embeddings:"):
+            batch_data = self.corpus[start_idx : start_idx + self.batch_size]["contents"]
+            batch_data = [f"{self.instruction}{doc}" for doc in batch_data]
+
+            inputs = self.tokenizer(
+                batch_data,
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+                max_length=self.max_length,
+            ).to("cuda")
+
+            inputs = {k: v.cuda() for k, v in inputs.items()}
+
+            # TODO: support encoder-only T5 model
+            if "T5" in type(self.encoder).__name__ or (self.gpu_num > 1 and hasattr(self.encoder, 'module') and "T5" in type(self.encoder.module).__name__):
+                # T5-based retrieval model
+                decoder_input_ids = torch.zeros((inputs["input_ids"].shape[0], 1), dtype=torch.long).to(
+                    inputs["input_ids"].device
+                )
+                output = self.encoder(**inputs, decoder_input_ids=decoder_input_ids, return_dict=True)
+                embeddings = output.last_hidden_state[:, 0, :]
+
+            else:
+                output = self.encoder(**inputs, return_dict=True)
+                embeddings = pooling(
+                    output.pooler_output, output.last_hidden_state, inputs["attention_mask"], self.pooling_method # type: ignore
+                )
+                embeddings = torch.nn.functional.normalize(embeddings, dim=-1)
+
+            embeddings = cast(torch.Tensor, embeddings)
+            embeddings = embeddings.detach().cpu().numpy()
+            all_embeddings.append(embeddings)
+
+        all_embeddings = np.concatenate(all_embeddings, axis=0)
+        all_embeddings = all_embeddings.astype(np.float32)
+
+        return all_embeddings
+
+    @torch.no_grad()
+    def build_dense_index(self):
+        """Obtain the representation of documents based on the embedding model(BERT-based) and
+        construct a faiss index.
+        """
+
+        if os.path.exists(self.index_save_path):
+            print("The index file already exists and will be overwritten.")
+
+        self.encoder, self.tokenizer = load_model(model_path=self.model_path, use_fp16=self.use_fp16)
+        hidden_size = self.encoder.config.hidden_size
+
+        if self.embedding_path is not None:
+            corpus_size = len(self.corpus)
+            all_embeddings = self._load_embedding(self.embedding_path, corpus_size, hidden_size)
+        else:
+            all_embeddings = self.encode_all()
+            if self.save_embedding:
+                self._save_embedding(all_embeddings)
+            del self.corpus
+
+        # build index
+        print("Creating index")
+        diskannpy.build_disk_index(
+            data=all_embeddings,
+            distance_metric="l2",
+            index_directory=self.index_save_path,
+            complexity=100, # default
+            graph_degree=64, # default
+            search_memory_maximum=self.search_mem_max,
+            build_memory_maximum=100, # use up to 100GB RAM for index build
+            num_threads=0, # use all available logical processors
+            pq_disk_bytes=0, # do not perform PQ compression
+        ) # type: ignore
+        print("Finish!")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Creating index.")
+
+    # Basic parameters
+    parser.add_argument("--model_path", type=str, default=None)
+    parser.add_argument("--corpus_path", type=str)
+    parser.add_argument("--save_dir", default="indexes/", type=str)
+
+    # Parameters for building dense index
+    parser.add_argument("--max_length", type=int, default=180)
+    parser.add_argument("--batch_size", type=int, default=512)
+    parser.add_argument("--use_fp16", default=False, action="store_true")
+    parser.add_argument("--search_mem_max", type=float, default=0.6)
+    parser.add_argument("--pooling_method", type=str, default=None)
+    parser.add_argument("--instruction", type=str, default=None)
+    parser.add_argument("--embedding_path", default=None, type=str)
+    parser.add_argument("--save_embedding", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    index_builder = DiskannIndexBuilder(
+        model_path=args.model_path,
+        corpus_path=args.corpus_path,
+        save_dir=args.save_dir,
+        max_length=args.max_length,
+        batch_size=args.batch_size,
+        use_fp16=args.use_fp16,
+        search_mem_max=args.search_mem_max,
+        pooling_method=args.pooling_method,
+        instruction=args.instruction,
+        embedding_path=args.embedding_path,
+        save_embedding=args.save_embedding,
+    )
+    index_builder.build_index()
+
+
+if __name__ == "__main__":
+    main()
